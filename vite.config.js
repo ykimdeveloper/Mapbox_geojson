a962adc54d3937c66e1bc69dd095e6c0b882b760
ykimdeveloper <ykimdeveloper@gmail.com>
2025-03-22 14:33:29 -0700

    gh-pages route and local dev paths
    paths for gh-pages and localhost
    
     base: '/Mapbox_geojson/',
      base: '/',

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// base for gh-pages
 export default defineConfig({
+  // base: '/Mapbox_geojson/',
   base: '/',
   plugins: [react()],
 })
