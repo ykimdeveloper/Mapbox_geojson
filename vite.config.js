c02819ed09785713a4df92ca23f9ecda4690fec6
ykimdeveloper <ykimdeveloper@gmail.com>
2025-03-24 22:31:35 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ export default defineConfig({
   // base: '/Mapbox_geojson/',
   base: '/',
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
+  
 })
